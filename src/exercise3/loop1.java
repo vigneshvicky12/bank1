4b4151c00eb752dce5982e82ad3412b2746e0af5
vigneshvicky12 <vigneshkanagaraj1@gmail.com>
2022-01-02 21:07:04 +0530

    first

---
+++ src/exercise3/loop1.java
@@ -0,0 +1,24 @@
+package exercise3;
+
+public class loop1 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		int b;
+		int c=1;
+		
+		for(a=1; a<=4; a++){
+			
+			for(b=1; b<=a; b++){
+				
+				System.out.print(c);
+				c++;
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
